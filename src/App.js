ec15095247132a39b95f807dd4b3a100c0f44b9f
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-11-22 21:24:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 // IMPORT useState
-import React from 'react';
+import React, {useState}from "react";
 import './styles.css';
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from './imageData.js';
+
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage] 
+  // USE useState TO CREATE  [bigImage, setBigImage]
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
 
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleClick = (img) => {
+    setBigImage(img);
+  };
 
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+    // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+  const images = imagesArr.map((image, index) => {
+    return (
+      <img
+        key={index}
+        src={image.img}
+        alt={image.city}
+        onClick={() => handleClick(image.img)}
+      />
+    );
+  });
+
 
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
         <div id="thumbnails">
-          {/* RENDER THE IMAGES ARRAY  */}
+          {images} 
         </div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        {<img id="bigImage" src={bigImage} alt="bigImage" />}
+        {<img src={bigImage} /> }
       </div>
     </div>
   );
